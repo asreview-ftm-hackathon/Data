be5f2eaad2e35c1c97942556774aeb0e6107d168
JT <j.j.teijema@uu.nl>
2021-11-26 12:43:11 +0100

    Update readme and file naming

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Data
 In the repository you can find the data to be used during the hackathon.
+
+Good luck!
+
+![logo](https://raw.githubusercontent.com/asreview/asreview-artwork/e2e6e5ea58a22077b116b9c3d2a15bc3fea585c7/SVGicons/IconELAS/ELASeyes24px24px.svg
+"ASReview")
+
+## Content
+
+This repository contains scripts for loading and sampling the data into parts.
+`load_data.py` loads the data and stores the file to the `data` folder.
+`sample_data.py` samples the data into a specified amount of parts.
--- scripts/sample_data.py
+++ scripts/sample_data.py
@@ -1,6 +1,9 @@
 # import pandas
 import pandas as pd
 
+# Settings
+num_samples = 10
+
 # Load the data
 print("Loading data...")
 df = pd.read_csv(r'..\data\allExport.csv')
@@ -9,12 +12,12 @@ df = pd.read_csv(r'..\data\allExport.csv')
 print("Scrambling data...")
 df = df.sample(frac=1).reset_index(drop=True)
 
-# split dataset into 10 different files
-print("Splitting data...")
-df_list = [df[i::10] for i in range(10)]
+# split dataset into num_samples different files
+print(f"Splitting data into {num_samples} parts...")
+df_list = [df[i::num_samples] for i in range(num_samples)]
 
-# save df_list to 10 different files
+# save df_list to num_samples different files
 print("Saving data...")
-for i in range(10):
+for i in range(num_samples):
     df_list[i].to_excel(f'..\\sampled data\\preprocessed_{i+1}.xlsx',
                         index=False)
